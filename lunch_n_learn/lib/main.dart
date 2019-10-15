f504f66ac44f96f11d60151d239b2a91f64c3bd2
duncan-iaria <duncaniaria@gmail.com>
2019-10-15 17:43:41 -0400

    🔧step 1

--- lunch_n_learn/lib/main.dart
+++ lunch_n_learn/lib/main.dart
@@ -15,9 +15,30 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(64.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: AssetImage("assets/pizza.png"),
+                ),
+                SizedBox(height: 32.0),
+                Text(
+                  "Welcome to Lunch",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
-
---
+++ lunch_n_learn/lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import './constants.dart';
+
+final ThemeData theme = ThemeData.light().copyWith(
+  primaryColor: kMainColor,
+);
